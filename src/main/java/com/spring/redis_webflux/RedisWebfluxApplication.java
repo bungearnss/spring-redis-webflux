1658e513763e2cb793a02859006476a3650b492c
bungearntun <love2554f@hotmail.com>
2025-03-06 13:15:25 +0700

    added : FibService

--- src/main/java/com/spring/redis_webflux/RedisWebfluxApplication.java
+++ src/main/java/com/spring/redis_webflux/RedisWebfluxApplication.java
@@ -2,8 +2,10 @@ package com.spring.redis_webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisWebfluxApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/spring/redis_webflux/service/FibService.java
@@ -0,0 +1,36 @@
+package com.spring.redis_webflux.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class FibService {
+
+    @Cacheable(value = "math:fib", key = "#index")
+    public int getFib(int index) {
+        log.info("calculating fib for {}", index);
+        return this.fib(index);
+    }
+
+    // PUT / POST / PATCH / DELETE
+    @CacheEvict(value = "math:fib", key = "#index")
+    public void clearCache(int index) {
+        log.info("clearing hash key");
+    }
+
+    //  @Scheduled(fixedRate = 10_000)
+    @CacheEvict(value = "math:fib", allEntries = true)
+    public void clearCache() {
+        log.info("clearing all fib keys");
+    }
+
+    //intentional 2^N
+    private int fib(int index) {
+        if (index < 2)
+            return index;
+        return fib(index - 1) + fib(index - 2);
+    }
+}
